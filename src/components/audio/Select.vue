429cd5da87923516d6a5e54c9f26c348117d8951
moffgato <op@xx>
2024-09-22 09:30:56 +0200

    fix 2-way binding for select component

--- src/components/audio/Select.vue
+++ src/components/audio/Select.vue
@@ -1,58 +1,72 @@
 <script setup lang="ts">
-import { watch } from 'vue'
+import { watch, ref } from 'vue';
 import {
   Select,
   SelectContent,
   SelectGroup,
-  SelectItem,
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
+  SelectItem,
+} from '@/components/ui/select';
 
-
-type audioOption = {
-  label: string,
-  value: string,
-}
-
+// Define props
+type AudioOption = {
+  label: string;
+  value: string;
+};
 
 const props = defineProps({
   audioOptions: {
-    type: Array<audioOption>,
-    default: () => []
+    type: Array as () => AudioOption[],
+    default: () => [],
   },
   modelValue: {
     type: String,
-    default: ''
-  }
-})
-
-
-const emit = defineEmits(['update:modelValue'])
-
-const handleChange = (value: string) => {
-  console.log({ changedAudioUrl: value })
-  emit('update:modelValue', value)
-}
-watch(() => props.modelValue, (newValue, oldValue) => {
-  console.log({ newValue, oldValue })
+    default: '',
+  },
 });
 
+const emit = defineEmits(['update:modelValue']);
+
+// Internal value to work with v-model
+const internalValue = ref(props.modelValue);
+
+// Watch internalValue and emit changes
+watch(internalValue, (newVal) => {
+  emit('update:modelValue', newVal);
+  console.log({ changedAudioUrl: newVal });
+});
+
+// Watch prop changes and update internalValue
+watch(
+  () => props.modelValue,
+  (newVal) => {
+    if (newVal !== internalValue.value) {
+      internalValue.value = newVal;
+    }
+  }
+);
 </script>
 
+
+
 <template>
-  <Select @valuechange="handleChange">
+  <!-- Custom Select component -->
+  <Select v-model='internalValue'>
     <SelectTrigger class="w-[180px]">
-      <SelectValue placeholder="Select a song" />
+      <!-- Display the currently selected value in the trigger -->
+      <SelectValue :value="modelValue" placeholder="Select a song" />
     </SelectTrigger>
     <SelectContent>
       <SelectGroup>
         <SelectLabel>Songs</SelectLabel>
+        <!-- Render available audio options and pass value to handleChange -->
         <SelectItem
           v-for="(option, index) in audioOptions"
           :key="index"
           :value="option.value"
+          class="select-none"
         >
           {{ option.label }}
         </SelectItem>
