4c156366c2a0462cb7322010b0d6b504a05fed72
moffgato <op@xx>
2024-09-22 09:32:31 +0200

    add singleton for audio context + add fetch,get,close fn()'s for audio context

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,12 +5,46 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export async function fetchAudioBuffer(audioContext: AudioContext, url: string): Promise<AudioBuffer> {
-  const response = await fetch(url)
-  if (!response.ok) {
-    throw new Error(`Failed to fetch audio at ${url}: ${response.statusText}`)
+
+let audioContextInstance: AudioContext | null = null
+
+export const getAudioContext = (): AudioContext => {
+  if (!audioContextInstance) {
+    audioContextInstance = new (window.AudioContext || (window as any).webkitAudioContext)()
+    console.log('AudioContext created.')
   }
-  const arrayBuffer = await response.arrayBuffer()
-  return await audioContext.decodeAudioData(arrayBuffer)
+  return audioContextInstance
 }
+
+export const closeAudioContext = async () => {
+  if (audioContextInstance) {
+    try {
+      await audioContextInstance.close()
+      console.log('AudioContext closed.')
+      audioContextInstance = null
+    } catch (error) {
+      console.error('Error closing AudioContext:', error)
+    }
+  }
+}
+
+export const fetchAudioBuffer = async (audioCtx: AudioContext, url: string): Promise<AudioBuffer> => {
+  try {
+    const response = await fetch(url)
+    console.log(`Fetching audio from: ${url}`, response)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const arrayBuffer = await response.arrayBuffer()
+    console.log('ArrayBuffer fetched successfully:', arrayBuffer)
+    const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer)
+    console.log('AudioBuffer decoded successfully:', audioBuffer)
+    return audioBuffer
+
+  } catch (error) {
+    console.error('Error fetching or decoding audio data:', error)
+    throw error
+  }
+}
+
 
